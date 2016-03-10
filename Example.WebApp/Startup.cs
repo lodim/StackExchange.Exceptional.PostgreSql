4313038c0c65a8d2605a20d08e211f37db490b72
Bogdan Mihai <mihai.bogdan.eugen@gmail.com>
2016-03-10 10:20:33 +0200

    added example web app using mvc5

---
+++ Example.WebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Example.WebApp.Startup))]
+namespace Example.WebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
